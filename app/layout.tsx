d90e1c58b713459c87a2702b03da715b80d76b46
Madhu H C <cmadh@cmadh-ltmy8rz.internal.salesforce.com>
2024-05-13 23:10:52 -0700

    Complete tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: {
+		template: "%s | Acme Dashboard",
+		default: "Acme Dashboard",
+	},
+	description: "The official Next.js Learn Dashboard built with App Router.",
+	metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
 	children,
@@ -8,6 +18,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<meta name="keywords" content="Invoice, Dashboard, Customers" />
+			<meta property="og:title" content="Expenses dashboard" />
+			<meta
+				property="og:description"
+				content="Manage customer invoices in single place"
+			/>
+			<meta property="og:image" content="/favicon.ico" />
+			<link rel="icon" href="/favicon.ico" />
 			<body className={`${inter.className} antialiased`}>{children}</body>
 		</html>
 	);
